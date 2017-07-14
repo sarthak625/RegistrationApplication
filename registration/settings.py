85c5497e182a4e82782fe4b9231019d7c62fb725
Sarthak <sarthakn18@gmail.com>
2017-07-14 11:31:26 +0530

    Deploy

--- registration/settings.py
+++ registration/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/example.com/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/account/'
 
